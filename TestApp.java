d84385cdfcdf2ae81bd2ea0ccf7279fc02f4ec84
PanosRCng <panosrcng@gmail.com>
2018-06-07 17:25:50 -0400

    most recent 100 tweets for the top10 trends in a woeid

--- TestApp.java
+++ TestApp.java
@@ -1,17 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
+import java.util.Collections;
 
 import twitter4j.RateLimitStatus;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.TwitterException;
 import twitter4j.Status;
+import twitter4j.api.TrendsResources;
 import twitter4j.api.UsersResources;
 
 import TwitterAnalytics.TwitterApi;
 import twitter4j.User;
 import twitter4j.ResponseList;
 import twitter4j.api.TimelinesResources;
+import twitter4j.api.TrendsResources;
+import twitter4j.Trends;
+import twitter4j.Trend;
 
 
 public class TestApp
@@ -85,11 +92,15 @@ public class TestApp
 		try
 		{
 			Query query = new Query(query_string);
+			query.setResultType(Query.RECENT);
+			query.setCount(100);
+
 			QueryResult result;
 
 			do
 			{
 				result = TwitterApi.instance().search(query);
+
 				List<Status> tweets = result.getTweets();
 
 				for(Status tweet : tweets)
@@ -108,13 +119,49 @@ public class TestApp
 	}
 
 
+	public List<Trend> getTopTrends(int woeid, int top)
+	{
+		try
+		{
+			TrendsResources trendsResources = TwitterApi.instance().trends();
+
+			Trends trends = trendsResources.getPlaceTrends(woeid);
+			Trend[] trendsArray = trends.getTrends();
+
+			return Arrays.asList(trendsArray).subList(0, 10);
+		}
+		catch(TwitterException te)
+		{
+			te.printStackTrace();
+			System.out.println("Failed to find trends: " + te.getMessage());
+		}
+
+		return Collections.emptyList();
+	}
+
+
+	public void getTrends(int woeid)
+	{
+		List<Trend> top10trends = this.getTopTrends(woeid, 10);
+
+		for(Trend trend : top10trends)
+		{
+			System.out.println("name: " + trend.getName() + ", query: " + trend.getQuery() + ", url: " + trend.getURL());
+
+			this.search( trend.getQuery() );
+		}
+	}
+
+
+
 	public static void main(String[] args)
 	{
 		TestApp testApp = new TestApp();
 
 		//testApp.showRateLimits();
 		//testApp.search("#GolGR");
-		testApp.findUsers("Kathimerini_gr");
+		//testApp.findUsers("Kathimerini_gr");
+		testApp.getTrends(23424833);
 
 		System.out.println("all ok");
 	}
